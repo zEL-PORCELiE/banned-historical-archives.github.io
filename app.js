99d9a97d29ca7dacecdda5d1b0d6d411bd7f2add
zEL-PORCELiE <zEL-PORCELiE@users.noreply.github.com>
2025-07-08 10:33:26 +0000

    chore: update app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'json'
+
+class main < Sinatra::Application
+  configure do
+    set :port, 3000
+    set :environment, :development
+  end
+
+  before do
+    content_type :json
+  end
+
+  get '/' do
+    {
+      message: 'Welcome to banned-historical-archives.github.io API',
+      version: '1.0.0',
+      timestamp: Time.now.iso8601
+    }.to_json
+  end
+
+  get '/health' do
+    {
+      status: 'healthy',
+      uptime: Process.clock_gettime(Process::CLOCK_MONOTONIC),
+      timestamp: Time.now.iso8601
+    }.to_json
+  end
+
+  get '/users' do
+    { users: [] }.to_json
+  end
+
+  post '/users' do
+    user_data = JSON.parse(request.body.read)
+    { user: user_data.merge(id: rand(1000), created_at: Time.now) }.to_json
+  end
+end
+
+main.run!
